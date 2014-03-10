81a9a1082eb69d4ea420e1519eccacb53e71ebca
Luiz Cezer <lccezinha@gmail.com>
2014-03-09 23:43:45 -0300

    métodos Game#ended e Game#next_step

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,19 @@ class Game
 
   def initialize(output = STDOUT)
     @output = output
+    @ended = false
   end
 
   def start
     @output.puts 'Bem vindo ao jogo da forca'
+  end
+
+  def ended?
+    @ended
+  end
+
+  def next_step
+    @output.puts 'Qual o tamanho da palavra a ser sorteada?'
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,31 @@ require 'spec_helper'
 require 'game'
 
 describe Game do
+  let(:output) { double 'output' } # Definindo um colaborador p/ Game
+  subject(:game) { Game.new output }
+
   context '#start' do
     it 'show welcome message' do
-      output = double 'output'
-      game = Game.new output
-
       message = 'Bem vindo ao jogo da forca'
       expect(output).to receive(:puts).with(message)
 
       game.start
+    end
+  end
+
+  context '#ended?' do
+    it 'returns false when the game just started' do
+      expect(game).not_to be_ended
+    end
+  end
+
+  context '#next_step' do
+    context 'when the game just started' do
+      it 'ask to player, how many letters for your word' do
+        question = 'Qual o tamanho da palavra a ser sorteada?'
+        expect(output).to receive(:puts).with(question)
+        game.next_step
+      end
     end
   end
 end
